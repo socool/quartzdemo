f722e0f3cfff738a0140e7282f028363c080988d
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2018-09-06 21:31:38 +0700

    init project

---
+++ src/main/java/com/bcorpse/quartzdemo/QuartzdemoApplication.java
@@ -0,0 +1,12 @@
+package com.bcorpse.quartzdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzdemoApplication.class, args);
+	}
+}
